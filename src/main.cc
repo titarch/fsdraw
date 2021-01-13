7ada7f9a7f23bafd677dd5f69900fde63a90b8cf
Baptiste Parsy <baptiste.parsy@epita.fr>
2021-01-13 16:26:22 +0100

    Add github

--- src/main.cc
+++ src/main.cc
@@ -7,7 +7,7 @@ int main() {
     settings.antialiasingLevel = 16;
 
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "sfml-fsdraw", sf::Style::Default, settings);
-    auto path = Path::from_plain("../res/paths/twitter.txt");
+    auto path = Path::from_plain("../res/paths/github.txt");
     auto chain = path.chain(151);
     chain.setPosition(960, 540);
 
