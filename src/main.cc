6ab71b079bd50fa60a7e47eed46189503e8973fe
Baptiste Parsy <baptiste.parsy@epita.fr>
2021-01-13 16:24:23 +0100

    Add twitter

--- src/main.cc
+++ src/main.cc
@@ -7,8 +7,8 @@ int main() {
     settings.antialiasingLevel = 16;
 
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "sfml-fsdraw", sf::Style::Default, settings);
-    auto path = Path::from_plain("../res/paths/note.txt");
-    auto chain = path.chain(100);
+    auto path = Path::from_plain("../res/paths/twitter.txt");
+    auto chain = path.chain(151);
     chain.setPosition(960, 540);
 
     while (window.isOpen()) {
